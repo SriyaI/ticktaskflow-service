de9ef4781226d6fd49cfa3b1caf26b1dad1c0eb9
sriya ivaturi <sriya@aihyr.com>
2024-10-27 14:41:13 +0530

    configuring vite

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 // src/index.js
 const express=require('express');
+const bodyParser=require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
